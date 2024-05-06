eeb57213082f52d507f423e1968d718be6f83369
youmulew <youmulew@gmail.com>
2024-05-06 14:23:20 +0800

    A 新项目初始化

---
+++ CrampedRoomOfDeathDemo/assets/scripts/battle.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('battle')
+export class battle extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
