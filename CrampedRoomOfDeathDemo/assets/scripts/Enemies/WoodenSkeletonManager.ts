6c9cb22c8483a525beddda37dde5344841f9e9a6
悠木 <466040947@qq.com>
2024-05-17 10:33:26 +0800

    A add Enemy Attack Animation/State

--- CrampedRoomOfDeathDemo/assets/scripts/Enemies/WoodenSkeletonManager.ts
+++ CrampedRoomOfDeathDemo/assets/scripts/Enemies/WoodenSkeletonManager.ts
@@ -1,4 +1,4 @@
-import { _decorator } from 'cc';
+import { _decorator, error } from 'cc';
 import { DIRECTION_ENUM, ENTITY_STATE_ENUM, ENTITY_TYPE_ENUM, EVENT_TYPE } from '../../Enums';
 import { EntityManager } from '../../Base/EntityManager';
 import { WoodenSkeletonStateMachine } from './WoodenSkeletonStateMachine';
@@ -25,6 +25,7 @@ export class WoodenSkeletonManager extends EntityManager {
 	registerEvents() {
 		EventManager.Instance.on(EVENT_TYPE.PLAYER_BORN, this.onChangeDirection, this);
 		EventManager.Instance.on(EVENT_TYPE.PLAYER_MOVE_END, this.onChangeDirection, this);
+		EventManager.Instance.on(EVENT_TYPE.PLAYER_MOVE_END, this.onAttack, this);
 	}
 
 	// 参数 isInit 确保在第一次加载时，执行改变朝向算法
@@ -39,23 +40,38 @@ export class WoodenSkeletonManager extends EntityManager {
 		const absDisX = Math.abs(disX);
 		const absDisY = Math.abs(disY);
 
-		console.log(disX, disY, absDisX, absDisY);
-
 		// 位于对角线时，不改变朝向
 		if (absDisX === absDisY && !isInit) return;
+		let targetDirection: DIRECTION_ENUM = this.direction;
 
 		if (disX >= 0 && disY >= 0) {
 			// 第一象限
-			this.direction = absDisX > absDisY ? DIRECTION_ENUM.RIGHT : DIRECTION_ENUM.TOP;
+			targetDirection = absDisX > absDisY ? DIRECTION_ENUM.RIGHT : DIRECTION_ENUM.TOP;
 		} else if (disX >= 0 && disY <= 0) {
 			// 第四象限
-			this.direction = absDisX > absDisY ? DIRECTION_ENUM.RIGHT : DIRECTION_ENUM.BOTTOM;
+			targetDirection = absDisX > absDisY ? DIRECTION_ENUM.RIGHT : DIRECTION_ENUM.BOTTOM;
 		} else if (disX <= 0 && disY >= 0) {
 			// 第二象限
-			this.direction = absDisX > absDisY ? DIRECTION_ENUM.LEFT : DIRECTION_ENUM.TOP;
+			targetDirection = absDisX > absDisY ? DIRECTION_ENUM.LEFT : DIRECTION_ENUM.TOP;
 		} else if (disX <= 0 && disY <= 0) {
 			// 第三象限
-			this.direction = absDisX > absDisY ? DIRECTION_ENUM.LEFT : DIRECTION_ENUM.BOTTOM;
+			targetDirection = absDisX > absDisY ? DIRECTION_ENUM.LEFT : DIRECTION_ENUM.BOTTOM;
+		}
+		// 只在朝向需要变化时设置，避免出现动画一直重置
+		if (targetDirection != this.direction) {
+			this.direction = targetDirection;
+		}
+	}
+
+	onAttack() {
+		if (!DataManager.Instance.player) {
+			error('未获取到玩家数据');
+			return;
+		}
+		const { x: playerX, y: playerY } = DataManager.Instance.player;
+		const distance = Math.abs(this.x - playerX) + Math.abs(this.y - playerY);
+		if (distance <= 1) {
+			this.state = ENTITY_STATE_ENUM.ATTACK;
 		}
 	}
 }
--- CrampedRoomOfDeathDemo/assets/scripts/Enemies/WoodenSkeletonStateMachine.ts
+++ CrampedRoomOfDeathDemo/assets/scripts/Enemies/WoodenSkeletonStateMachine.ts
@@ -6,6 +6,7 @@ import { EntityManager } from '../../Base/EntityManager';
 const { ccclass, property } = _decorator;
 
 const IDLE_URL = 'texture/woodenskeleton/idle';
+const ATTACK_URL = 'texture/woodenskeleton/attack';
 
 @ccclass('WoodenSkeletonStateMachine')
 export class WoodenSkeletonStateMachine extends StateMachine {
@@ -21,8 +22,9 @@ export class WoodenSkeletonStateMachine extends StateMachine {
 	}
 
 	initParams() {
-		this.params.set(PARAMS_NAME_ENUM.IDLE, getInitParamsTrigger());
 		this.params.set(PARAMS_NAME_ENUM.DIRECTION, getInitParamsNumber());
+		this.params.set(PARAMS_NAME_ENUM.IDLE, getInitParamsTrigger());
+		this.params.set(PARAMS_NAME_ENUM.ATTACK, getInitParamsTrigger());
 	}
 
 	initStateMachines() {
@@ -30,12 +32,12 @@ export class WoodenSkeletonStateMachine extends StateMachine {
 			PARAMS_NAME_ENUM.IDLE,
 			new DirectionSubStateMachine(this, IDLE_URL, AnimationClip.WrapMode.Loop),
 		);
+		this.stateMachines.set(PARAMS_NAME_ENUM.ATTACK, new DirectionSubStateMachine(this, ATTACK_URL));
 	}
 
 	initAnimationEvents() {
 		this.animationComponent.on(Animation.EventType.FINISHED, () => {
 			const name = this.animationComponent.defaultClip.name;
-			//const whiteList = ['turn'];
 			if (!name.includes('idle')) {
 				this.node.getComponent(EntityManager).state = ENTITY_STATE_ENUM.IDLE;
 			}
@@ -46,11 +48,11 @@ export class WoodenSkeletonStateMachine extends StateMachine {
 		if (this.currentState === this.stateMachines.get(PARAMS_NAME_ENUM.DIRECTION)) {
 			this.currentState = this.stateMachines.get(PARAMS_NAME_ENUM.IDLE);
 		} else {
-			if (this.params.get(PARAMS_NAME_ENUM.IDLE).value) {
-				this.currentState = this.stateMachines.get(PARAMS_NAME_ENUM.IDLE);
+			if (this.params.get(PARAMS_NAME_ENUM.ATTACK).value) {
+				this.currentState = this.stateMachines.get(PARAMS_NAME_ENUM.ATTACK);
 			} else {
-				// 确保 set 被触发
-				this.currentState = this.currentState;
+				// 默认为 IDLE 状态
+				this.currentState = this.stateMachines.get(PARAMS_NAME_ENUM.IDLE);
 			}
 		}
 	}
